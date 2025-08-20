43e40e1b85abf66437b69934c888ff24a5d1171e
Berny <berenis.fopa@gmail.com>
2025-08-20 15:27:39 -0400

    added the menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Grid from "./Components/Grid.jsx";
+
 function App() {
 
 
@@ -7,7 +8,23 @@ function App() {
     <>
       <h1>Pathfinding visualizer</h1>
 
+        <div className='menu'>
+        <label > DFS
+        <input type="radio" name="type"/>
+        </label>
+        <label> BFS
+        <input type="radio" name="type"/>
+        </label>
+        <div className="choices">
+        <button>Set Start</button>
+        <button>Set End</button>
+        <button>Set walls</button>
+        <button>RUN</button>
+        </div>
+        </div>
+
         <Grid/> {/*self contained component */}
+
     </>
   )
 }
