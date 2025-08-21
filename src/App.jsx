6b5823fb1186c4bfcaf62f9d7d72348adf9cd9c4
Berny <berenis.fopa@gmail.com>
2025-08-21 07:58:50 -0400

    handled the js to make all selections before starting the search

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ function App() {
     const[startChoice, editStartChoice] = useState(false)
     const[endChoice, editEndChoice] = useState(false);
     const [setObs, editSetObs] = useState(false)
+    const [setRun, editSetRun] = useState(false)
+    const [searchType, editSearchType] = useState("DFS")
 
     function handleStartChoice(){
         editSetObs(false)
         editEndChoice(false)
+        editSetRun(false)
         const start = startChoice === false
         editStartChoice(start)
     }
@@ -16,6 +19,7 @@ function App() {
     function handleEndChoice() {
         editSetObs(false)
         editStartChoice(false)
+        editSetRun(false)
         const end = endChoice === false
         editEndChoice(end)
     }
@@ -23,35 +27,47 @@ function App() {
     function handleEditObs() {
         editEndChoice(false)
         editStartChoice(false)
+        editSetRun(false)
         const obs = setObs === false
         editSetObs(obs)
     }
 
+    function handleRun(){
+        editEndChoice(false)
+        editStartChoice(false)
+        editSetObs(false)
+        const tempRun = setRun === false;
+        editSetRun(tempRun)
+    }
 
-
-
+    function handleDfs(){
+        editSearchType('DFS')
+    }
+    function handleBfs(){
+        editSearchType('BFS')
+    }
 
     return (
     <>
       <h1>Pathfinding visualizer</h1>
 
         <div className='menu'>
-        <label > DFS
-        <input type="radio" name="type"/>
+        <label onClick={handleDfs}> DFS
+        <input  id="DFS" type="radio" name="searchType"/>
         </label>
-        <label> BFS
-        <input type="radio" name="type"/>
+        <label onClick={handleBfs}> BFS
+        <input id="BFS" type="radio" name="searchType"/>
         </label>
         <div className="choices">
         <button className={`${startChoice ? "startStyling" : ""} `} onClick={handleStartChoice}>Set Start</button>
         <button className={`${endChoice ? "endStyling" : ""}`} onClick={handleEndChoice}>Set End</button>
         <button className={`${setObs ? "obsStyling" : ""}`} onClick={handleEditObs}>Set Obstacles</button>
-        <button>RUN</button>
+        <button className={ `${setRun ? "runStyling": ""}`} onClick={handleRun}>RUN</button>
         <button onClick={()=> location.reload()}>RESET</button>
         </div>
         </div>
 
-        <Grid setStartChoice={startChoice} setEndChoice={endChoice} setObs={setObs}/> {/*self contained component */}
+        <Grid setStartChoice={startChoice} setEndChoice={endChoice} setObs={setObs} setRun={setRun} setSearchType={searchType}/> {/*self contained component */}
 
     </>
   )
--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -11,14 +11,14 @@ const COLS = 50;
 
 // helper function to create the initial grid data structure
 // we need it to be based on the function since it occurs when the page is loaded
-const initializeGrid = () =>{
+function initializeGrid() {
     const grid  = [];
 
     for (let row = 0 ; row < ROWS; row++){
         const CurrentRow = [];
         for (let col  = 0 ; col < COLS;  col++){
-            // so each node does have its own properties (its an object) and we've just defined it using its positoin
-            // we aadding to eah row all the column nodes associated to that row
+            // so each node does have its own properties (it's an object) and we've just defined it using its position
+            // we're adding to eah row all the column nodes associated to that row
             CurrentRow.push(
                 // we are pushing an object
                 {
@@ -37,17 +37,29 @@ const initializeGrid = () =>{
     }
 
     return grid;
-};
+}
 
 
-const Grid = ({setStartChoice,setEndChoice,setObs})=> {// correct name is needed here
+function DFS(start,end,grid){
+
+}
+function BFS(start,end,grid){
+
+}
+
+const Grid = ({setStartChoice,setEndChoice,setObs,setRun,setSearchType})=> {// correct name is needed here
     // use state to store the grid and the edit function associated with it
     const[grid, setGrid] = useState([]) // the grid begins empty but then we associate the initialize to it
-
     const [startNode, setStartNode] = useState({row:null,col:null});
     const [endNode, setEndNode] = useState({row:null,col:null});
 
-   // if the node is clicked it tells it to the grid through on lick
+    // use effect used to run code only when we mount the component so we use it to initialize the grid
+    useEffect(()=>{
+        setGrid(initializeGrid());
+    },[]) // the empty array as dependency means that it only runs one since the array can never change
+
+
+    // if the node is clicked it tells it to the grid through on lick
     function HandleNodeClick(row,col){
         if (setStartChoice) {
             const newGrid = grid.map(gridRow => gridRow.slice()); // slice used to copy here
@@ -93,14 +105,16 @@ const Grid = ({setStartChoice,setEndChoice,setObs})=> {// correct name is needed
         }
     }
 
+    if (startNode.row !== null && endNode.col !== null) {
+        if (setRun){
+            if (setSearchType === 'DFS'){
+                DFS(startNode,endNode,grid)
+            }else if (setSearchType === "BFS"){
+                BFS(startNode,endNode,grid)
+            }
+        }
 
-
-
-    // use effect used to run code only when we mount the component so we use it to initialize the grid
-    useEffect(()=>{
-        setGrid(initializeGrid());
-    },[]) // the empty array as dependency means that it only runs one since the array can never change
-
+    }
 
     return (
         <div className='grid'>
